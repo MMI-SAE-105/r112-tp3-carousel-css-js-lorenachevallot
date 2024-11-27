02212c96f5a370b222b51e432af7ca1a65a14a3d
lorenachevallot <loni.chevallot@gmail.com>
2024-11-27 09:42:23 +0100

    ajout const script js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,5 @@
 // *** CAROUSEL ***
 // TODO
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+const container = document.querySelector(".carousel__container");
