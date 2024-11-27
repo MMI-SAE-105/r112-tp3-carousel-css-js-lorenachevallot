91d6fc2a6dd5a6073bbb1a5668a7b3d6bbad0e1a
lorenachevallot <loni.chevallot@gmail.com>
2024-11-27 09:44:42 +0100

    ajout prevbutton

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,7 @@
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 const container = document.querySelector(".carousel__container");
+
+prevButton.addEventListener("click", () => {
+    container.scrollBy({ left: -300, behavior: "smooth" }); // Faites défiler vers la gauche
+});
